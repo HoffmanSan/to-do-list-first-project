5fd37b5ed57bef8bd43d95474d9ef32269235b81
HoffmanSan <gortychmaciej97@gmail.com>
2023-08-10 15:20:26 +0200

    added TaskList.js/css files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
+// imports
+import { nanoid } from 'nanoid'
+import { useState } from 'react';
+
 // styles
 import './App.css';
 
 // components
 import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList';
+
+const CURRENT_TASKS = [
+  {id: nanoid(), name: 'Eat', completed: false},
+  {id: nanoid(), name: 'Sleep', completed: false},
+  {id: nanoid(), name: 'Repeat', completed: false}
+]
 
 function App() {
+  const [currentTasks, setCurrentTasks] = useState(CURRENT_TASKS)
+
+  const toggleTaskCompleted = (id) => {
+    const completeToggledTasks = currentTasks.map(task => {
+      if(task.id === id){
+        return { ...task, completed: !task.completed};
+      }
+        return task;
+    });
+    setCurrentTasks(completeToggledTasks);
+  }
+
+  const handleDelete = (id) => {
+    const tasksAfterDeletion = currentTasks.filter(task => {
+      if(task.id === id){
+        return false;
+      }
+        return true;
+    });
+    setCurrentTasks(tasksAfterDeletion);
+  }
+
   return (
     <div className="App">
       <h1>Bucket List</h1>
       <TaskForm />
+      <TaskList currentTasks={currentTasks} toggleTaskCompleted={toggleTaskCompleted} handleDelete={handleDelete}/>
     </div>
   );
 }
